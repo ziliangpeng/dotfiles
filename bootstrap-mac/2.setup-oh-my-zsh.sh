4c878edd9c778e9c51e642a7162dcdd280b7ff54
Victor Peng <victor.peng@airbnb.com>
2016-10-15 20:18:13 -0700

    simplified setup-oh-my-zsh

--- bootstrap-mac/2.setup-oh-my-zsh.sh
+++ bootstrap-mac/2.setup-oh-my-zsh.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
 cd ~
-echo "Removing old ~/.oh-my-zsh ---------------------"
-rm -rf .oh-my-zsh
-rm -rf ~/.oh-my-zsh-custom
-
 echo "Installing oh-my-zsh... -----------------------"
-# Manually install
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-echo "Installing customs -------------------------------"
-git clone --recursive git@git.musta.ch:victor-peng/vpzsh.git .oh-my-zsh-custom
+# Step-by-step install
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/oh-my-zsh #conflict if repo already pulled
 
 echo "Setting up .zshrc -----------------------------"
 rm ~/.zshrc
-ln -s $HOME/Dropbox/config/oh-my-zsh-config/.zshrc $HOME/.zshrc
-
-echo "Putting submodule back to master branch -------"
-cd ~/.oh-my-zsh-custom
-git submodule foreach git checkout master
-
-
+ln -s $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
 
 if [ "/bin/zsh" != $SHELL ]; then
   echo "Change shell ----------------------------------"
