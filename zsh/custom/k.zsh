7a469a9c74910e7b54add830d54cc4137d46112f
Victor Peng <zpeng@nuro.ai>
2019-04-29 12:50:35 -0700

    more k alias

--- zsh/custom/k.zsh
+++ zsh/custom/k.zsh
@@ -7,6 +7,17 @@ alias kcclusters="kc get-clusters"
 alias kg="k get"
 alias kd="k describe"
 
+alias klocal="kc use-context kubernetes-admin@kubernetes"
+alias kcloud="kc use-context gke_nuro-ai_us-west1-a_jie-cluster-1"
+alias ktest="kc use-context gke_nuro-ai_us-west1-a_victor-test-cluster-1"
+alias kls-b="kc use-context gke_nuro-ai_us-west1-b_large-scale-cluster-1b"
+alias kls-c="kc use-context gke_nuro-ai_us-west1-c_large-scale-cluster-1c"
+
+alias gke-auth="gcloud container clusters get-credentials jie-cluster-1 --zone=us-west1-a"
+alias gssh='gcloud compute --project nuro-ai ssh --zone us-west1-a'
+alias gsshb='gcloud compute --project nuro-ai ssh --zone us-west1-b'
+alias gsshc='gcloud compute --project nuro-ai ssh --zone us-west1-c'
+
 alias kinfo="k cluster-info"
 func knodes() { k get nodes | grep "$1" }
 func kpods() { k get pods | grep "$1" }
