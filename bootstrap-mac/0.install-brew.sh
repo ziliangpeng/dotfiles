e64e4119283132cfe188cee4759c241d63dec79a
Victor Z. Peng <ziliangdotme@gmail.com>
2023-05-24 22:09:15 -0700

    no need to install cask

--- bootstrap-mac/0.install-brew.sh
+++ bootstrap-mac/0.install-brew.sh
@@ -8,7 +8,9 @@ xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Install Cask
-brew tap caskroom/cask
+# NOTE(20230524): cask is already part of brew, no need to install.
+# If you run `brew install cask`, it will install a emacs package management tool, which is not Cask.
+# brew tap caskroom/cask
 
 brew update
 brew tap homebrew/bundle
